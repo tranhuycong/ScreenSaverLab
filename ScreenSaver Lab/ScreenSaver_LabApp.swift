68eb2d3a9808832d18e3026e86f4e45f03c79ef3
congtranit <congtranit@gmail.com>
2024-12-09 14:18:35 +0700

    Initial Commit

---
+++ ScreenSaver Lab/ScreenSaver_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSaver_LabApp.swift
+//  ScreenSaver Lab
+//
+//  Created by Tran Cong on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSaver_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenSaver LabTests/ScreenSaver_LabTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSaver_LabTests.swift
+//  ScreenSaver LabTests
+//
+//  Created by Tran Cong on 9/12/24.
+//
+
+import Testing
+@testable import ScreenSaver_Lab
+
+struct ScreenSaver_LabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
